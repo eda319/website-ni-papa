7aec98cca73924f642a12c799145fe774771f905
Earl Areglo <edareglo319@gmail.com>
2026-05-22 18:07:36 +0800

    Added structure for the projects, and home sections

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,26 @@ accordionHeaders.forEach((header) => {
     content.classList.toggle("show");
   });
 });
+
+function copyEmail() {
+  navigator.clipboard.writeText("elexcellence@gmail.com");
+  alert("Email copied!");
+}
+
+function moveSlide(button, direction) {
+  const carousel = button.closest("[data-carousel]");
+  const slides = carousel.querySelectorAll(".slide");
+
+  let currentIndex = Array.from(slides).findIndex((s) =>
+    s.classList.contains("active"),
+  );
+
+  slides[currentIndex].classList.remove("active");
+
+  let nextIndex = currentIndex + direction;
+
+  if (nextIndex < 0) nextIndex = slides.length - 1;
+  if (nextIndex >= slides.length) nextIndex = 0;
+
+  slides[nextIndex].classList.add("active");
+}
